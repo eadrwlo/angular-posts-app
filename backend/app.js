0eb9ad6da618a3e768a6c3fb996db80dda4c04d9
eadrwlo <wlodek14a@gmail.com>
2019-01-07 21:27:31 +0100

    Add photo thumbs

--- backend/app.js
+++ backend/app.js
@@ -11,8 +11,10 @@ app.use((req, res, next) => {
 
 app.use('/api/posts' , (req, res, next) => {
   const posts = [
-    { id: 'asdasdasd12', title: 'First serv post', content: 'First post content'},
-    { id: 'asdasdasd12', title: 'Second serv post', content: 'Second post content'},
+    { id: 'asdasdasd11', title: 'Photo1', content: 'Wood', imageUrl: 'https://s3.us-east-2.amazonaws.com/photo-rotator-storage/photos/wood.jpg'},
+    { id: 'asdasdasd12', title: 'Photo2', content: 'Sea', imageUrl: 'https://s3.us-east-2.amazonaws.com/photo-rotator-storage/photos/sea.jpg'},
+    { id: 'asdasdasd13', title: 'Photo3', content: 'Forest', imageUrl: 'https://s3.us-east-2.amazonaws.com/photo-rotator-storage/photos/forest.jpg'},
+    { id: 'asdasdasd14', title: 'Photo4', content: 'Mountain', imageUrl: 'https://s3.us-east-2.amazonaws.com/photo-rotator-storage/photos/mountain.jpg'}
   ];
   res.status(200).json({
     message: 'Post fetched successfully',
